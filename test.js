714d670dab04ec152b263d1436e6807651415e09
David Handel <handeld@gmail.com>
2013-03-09 20:26:09 -0500

    Second commit

---
+++ test.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: davidhandel
+ * Date: 3/9/13
+ * Time: 8:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+console.log("hello world");
+console.log("hello hello");
+alert("help");
+
